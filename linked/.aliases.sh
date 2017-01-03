bc150c2e7473b783bc1295ce4fe12781cdaef8be
AlvarBer <alvaro.garcia95@hotmail.com>
2017-01-03 19:07:15 +0000

    Added alias

--- linked/.aliases.sh
+++ linked/.aliases.sh
@@ -17,3 +17,6 @@ fi
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+# Handy
+alias remake="make clean && make"
+
