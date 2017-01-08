f97dbc0e5298ac8235c44d6d6b93dba088810ed5
AlvarBer <alvaro.garcia95@hotmail.com>
2017-01-08 15:49:38 +0000

    Improved install scripts

--- install/arch.sh
+++ install/arch.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-# Please upgrade system before
-yaourt -Syyu
+# System upgrade
 sudo pacman -Syyu
+sudo yaourt -Syua --devel
 
+# Programs
 sudo pacman -S atom --no-confirm
-yaourt -S sublime-text-dev --no-confirm
-yaourt -S vim-plug-git --no-confirm
+yaourt -S byobu
+yaourt -S sublime-text-dev
 
+# Extras
+yaourt -S vim-plug-git
+yaourt -S autoenv
+
+# ElementaryOS-like notifications
 cd /usr/local/bin
 sudo git clone https://github.com/ihashacks/notifyosd.zsh
+
--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-# Add sublime text PPA and install it
+# System upgrade
+sudo apt-get update && sudo apt-get upgrade
+
+# Add sublime text PPA
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
+
+# Programs installation
 sudo apt-get install sublime-text-installer -y
-
-# Installs htop
 sudo apt-get install htop -y
-
-# Install byobu
 sudo apt-get install byobu -y
 
-# Python Installation
+# Extras installation
 git clone http://github.com/kennethreitz/autoenv.git ~/.autoenv
+# Vim plug installation
 
 # After-Install GHCi fix
 chmod g-w ~/.ghc
 chmod g-w ~/.ghc/ghci.conf
-
-# General upgrade
-sudo apt-get upgrade
 
